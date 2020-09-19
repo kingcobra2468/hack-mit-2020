66ff998d3dda4aa70a2b6577e599c314b88d9c41
Albert Stanley <albertjstanley@gmail.com>
2020-09-19 01:52:19 -0500

    tiny face detector fun

--- client/public/js/client.js
+++ client/public/js/client.js
@@ -1,13 +1,44 @@
-var name; 
-var connectedUser;
-var localVideo = document.querySelector('#localVideo'); 
+// var name; 
+// var connectedUser;
+// var localVideo = document.querySelector('#localVideo'); 
 
-navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(function(mediaStream) {
-    localVideo.srcObject = mediaStream;
-    localVideo.onloadedmetadata = function(e) {
-        localVideo.play();
-    };
+// navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(function(mediaStream) {
+//     localVideo.srcObject = mediaStream;
+//     localVideo.onloadedmetadata = function(e) {
+//         localVideo.play();
+//     };
     
-})
+// })
 
+const video = document.getElementById('localVideo')
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/static/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/static/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/static/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/static/models')
+]).then(startVideo)
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.videoWidth, height: video.videoHeight }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  }, 100)
+})
 
