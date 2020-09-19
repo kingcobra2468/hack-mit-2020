84348a53167c8884d8fd91b12994046957a3cab6
kingcobra2468 <eriktyryshkin@gmail.com>
2020-09-18 20:43:08 -0700

    Fix to skeleton

--- server/index.js
+++ server/index.js
@@ -7,11 +7,16 @@ const http = require("http");
 const server = http.createServer(app);
 
 const io = require("socket.io")(server);
-//app.use(express.static(__dirname + "/public"));
+app.use(express.static("/public"));
 
-io.on('connect', socket => {
-    socket.join('')    
+io.on('connection', socket => {
+    socket.join('game-session'); 
+    console.log("Made socket connection");   
 })
 
-io.sockets.on("error", e => console.log(e));
+io.on('test', socket => {
+    console.log("Made test connection");   
+})
+
+//io.sockets.on("error", e => console.log(e));
 server.listen(port, () => console.log(`Server is running on port ${port}`));
