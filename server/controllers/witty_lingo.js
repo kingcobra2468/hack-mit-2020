d673f5380e9046e95c37cc175e591a533be90d29
kingcobra2468 <eriktyryshkin@gmail.com>
2021-03-12 23:24:34 -0800

    reformatted code + removed dead code

--- server/controllers/witty_lingo.js
+++ server/controllers/witty_lingo.js
@@ -1,18 +1,17 @@
-module.exports = function(io) {
-    
+module.exports = function (io) {
+
     const express = require('express')
     const router = express.Router()
-    const {word_generator} = require('../generators/word_generator')
+    const { word_generator } = require('../generators/word_generator')
 
-    router.get('/new-game', function(req, res) { // home page
-        
-        //const {num_socket_connections} = require('../sockets/webrtc_socket')
+    router.get('/new-game', function (req, res) { // home page
 
         console.log('requested new game')
-        let ids = Object.keys(io.sockets.clients().connected);
-        let words = word_generator(ids.length, './word_banks/vocab-set-spanish-1.txt') //`${process.env.PROJECT_ROOT_DIR}/word_banks/vocab-set-spanish-1.txt`  
-        let word_bindings = {};
         
+        let ids = Object.keys(io.sockets.clients().connected);
+        let words = word_generator(ids.length, './word_banks/vocab-set-spanish-1.txt')
+        let word_bindings = {};
+
         ids.forEach((id, i) => word_bindings[id] = words[i])
         console.log(word_bindings)
         io.sockets.emit('new-witty-lingo-game', word_bindings)
@@ -20,22 +19,16 @@ module.exports = function(io) {
         res.json(word_bindings)
     })
 
-    router.get('/num-players', function(req, res) { // home page
-        
-        //const {num_socket_connections} = require('../sockets/webrtc_socket')
+    router.get('/num-players', function (req, res) { // home page
 
-        let ids = Object.keys(io.sockets.clients().connected);       
-        console.log(ids)
-        res.json({"num_players" : ids.length})
+        let ids = Object.keys(io.sockets.clients().connected);
+        res.json({ "num_players": ids.length })
     })
 
-    router.get('/players-ids', function(req, res) { // home page
-        
-        //const {num_socket_connections} = require('../sockets/webrtc_socket')
+    router.get('/players-ids', function (req, res) { // home page
 
-        let ids = Object.keys(io.sockets.clients().connected);       
-
-        res.json({"players_ids" : ids})
+        let ids = Object.keys(io.sockets.clients().connected);
+        res.json({ "players_ids": ids })
     })
 
     return router;
--- server/generators/word_generator.js
+++ server/generators/word_generator.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-module.exports.word_generator = function word_generator (num_words, file) {
-    
+module.exports.word_generator = function word_generator(num_words, file) {
+
     let selected_words = []
     let words = fs.readFileSync(file, 'utf8').split('\n');
 
